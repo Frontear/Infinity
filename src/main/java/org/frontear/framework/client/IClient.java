d8d245ab08044c5db49887e16b01a2c660144318
Ali Rizvi <frontear4594@gmail.com>
2019-08-11 11:19:15 -0400

    Implement JavaDocs
    As of now, they'll stay only for the framework.

--- src/main/java/org/frontear/framework/client/IClient.java
+++ src/main/java/org/frontear/framework/client/IClient.java
@@ -4,10 +4,25 @@ import org.frontear.framework.config.IConfig;
 import org.frontear.framework.info.IModInfo;
 import org.frontear.framework.logger.ILogger;
 
+/**
+ * Represents a client object. This will be loaded from {@link org.frontear.ForgeMod}
+ */
 public interface IClient {
+	/**
+	 * Contains all the important information loaded from the mcmod.info file
+	 * @return {@link IModInfo}
+	 */
 	IModInfo getModInfo();
 
+	/**
+	 * A basic logging system, backed by {@link org.apache.logging.log4j.Logger}
+	 * @return {@link ILogger}
+	 */
 	ILogger getLogger();
 
+	/**
+	 * A simple yet very effective configuration system which will contain all your important data
+	 * @return {@link IConfig}
+	 */
 	IConfig getConfig();
 }
--- src/main/java/org/frontear/framework/config/IConfigurable.java
+++ src/main/java/org/frontear/framework/config/IConfigurable.java
@@ -1,8 +1,20 @@
 package org.frontear.framework.config;
 
+/**
+ * The interface for all configurable objects which are managed via the {@link IConfig}
+ * @param <C> the object that is currently extending {@link IConfigurable}
+ */
 public interface IConfigurable<C extends IConfigurable<C>> {
+	/**
+	 * This is called when a property with the same name as {@link IConfigurable#getName()} is found from {@link IConfig#load()}
+	 * @param self the object that was serialized by {@link IConfig}
+	 */
 	void load(final C self);
 
+	/**
+	 * Represents the name that is found from the json properties
+	 * @return the name of the json property
+	 */
 	default String getName() {
 		return this.getClass().getSimpleName();
 	}
