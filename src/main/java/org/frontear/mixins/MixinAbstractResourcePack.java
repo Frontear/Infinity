11e5785cb0dbfeb6bcf45bac79cec0e30c959f62
Ali Rizvi <frontear4594@gmail.com>
2019-08-26 12:54:19 -0400

    Fix resource packs failing to load
    What an odd bug. I must investigate, since this feels like such an unecessary Overwrite

--- src/main/java/org/frontear/mixins/MixinAbstractResourcePack.java
+++ src/main/java/org/frontear/mixins/MixinAbstractResourcePack.java
@@ -1,13 +1,13 @@
 package org.frontear.mixins;
 
+import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.resources.AbstractResourcePack;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.*;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Mixin(AbstractResourcePack.class) public abstract class MixinAbstractResourcePack {
 	private static final int SIZE = 64; // the scaled resolution size
@@ -15,13 +15,19 @@ import java.awt.image.BufferedImage;
 	/**
 	 * @author prplz Please see https://prplz.io/memoryfix/ for more information
 	 */
-	@Inject(method = "getPackImage",
-			at = @At("RETURN")) private void getPackImage(CallbackInfoReturnable<BufferedImage> info) {
-		final BufferedImage image = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
-		final Graphics2D graphics = image.createGraphics();
-		graphics.drawImage(info.getReturnValue(), 0, 0, SIZE, SIZE, null);
-		graphics.dispose();
+	@Overwrite public BufferedImage getPackImage() throws IOException {
+		final BufferedImage image = TextureUtil.readBufferedImage(this.getInputStreamByName("pack.png"));
+		if (image != null) {
+			final BufferedImage scaled = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+			final Graphics2D graphics = image.createGraphics();
+			graphics.drawImage(image, 0, 0, SIZE, SIZE, null);
+			graphics.dispose();
 
-		info.setReturnValue(image);
+			return scaled;
+		}
+
+		return null;
 	}
+
+	@Shadow protected abstract InputStream getInputStreamByName(String name) throws IOException;
 }
