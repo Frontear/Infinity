1d9b23b9ffd7b06ca4e2f5048126528849e794e8
Ali Rizvi <frontear4594@gmail.com>
2019-09-11 17:32:36 -0400

    Add Unsafe into the Agent
    Because why not, frankly?

--- src/main/java/org/frontear/MinecraftAgent.java
+++ src/main/java/org/frontear/MinecraftAgent.java
@@ -1,13 +1,20 @@
 package org.frontear;
 
 import lombok.Getter;
+import lombok.SneakyThrows;
+import sun.misc.Unsafe;
 
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
 
 public final class MinecraftAgent {
 	@Getter private static Instrumentation instrumentation;
+	@Getter private static Unsafe unsafe;
 
-	public static void agentmain(String args, Instrumentation inst) {
+	@SneakyThrows({ NoSuchFieldException.class, IllegalAccessException.class }) public static void agentmain(String args, Instrumentation inst) {
 		instrumentation = inst;
+		final Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+		theUnsafe.setAccessible(true);
+		unsafe = (Unsafe) theUnsafe.get(null);
 	}
 }
