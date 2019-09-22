3af1cb7fdec72825b6cbb73ac1c7138bf55db702
Ali Rizvi <frontear4594@gmail.com>
2019-09-22 14:06:38 -0400

    Revert MixinGuiMultiplayer refresh change
    It's causing crashes, and failing to refresh. I'll address this much later, as it's such a low priority feature.

--- src/main/java/org/frontear/mixins/impl/MixinGuiMultiplayer.java
+++ src/main/java/org/frontear/mixins/impl/MixinGuiMultiplayer.java
@@ -1,22 +1,18 @@
 package org.frontear.mixins.impl;
 
-import lombok.var;
 import net.minecraft.client.gui.GuiMultiplayer;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.multiplayer.ServerList;
-import net.minecraft.client.network.OldServerPinger;
 import org.frontear.framework.threading.InfiniteThread;
 import org.frontear.framework.utils.time.TimeUnit;
 import org.frontear.framework.utils.time.Timer;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMultiplayer.class) public abstract class MixinGuiMultiplayer extends GuiScreen {
 	private static final Timer timer = new Timer();
-	@Shadow private ServerList savedServerList;
-	@Shadow @Final private OldServerPinger oldServerPinger;
 	private Thread refresh;
 
 	@Inject(method = "<init>",
@@ -31,15 +27,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 		refresh.start();
 	}
 
-	/**
-	 * @author Frontear
-	 * @reason Optimizations (I mean really, a new screen each time?)
-	 */
-	@Overwrite private void refreshServerList() {
-		for (var i = 0; i < savedServerList.countServers(); i++) {
-			oldServerPinger.ping(savedServerList.getServerData(i));
-		}
-	}
+	@Shadow protected abstract void refreshServerList();
 
 	@Inject(method = "onGuiClosed",
 			at = @At("TAIL")) private void onGuiClosed(CallbackInfo info) {
