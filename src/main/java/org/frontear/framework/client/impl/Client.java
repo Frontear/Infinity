62a32a3e2c493efd2baf1821c074a3c0245dd37c
Ali Rizvi <frontear4594@gmail.com>
2019-12-03 09:46:13 -0500

    Remove the weird debug loading
    It doesn't make sense to change up so much simply for debugging. The user should be able to understand what it means to debug, enabling it is not accidental.

--- src/main/java/org/frontear/framework/client/impl/Client.java
+++ src/main/java/org/frontear/framework/client/impl/Client.java
@@ -38,9 +38,7 @@ public abstract class Client implements IClient {
      * This is the expected/recommended working directory for the client. Should always point to the
      * minecraft data folder
      */
-    public static final String WORKING_DIRECTORY =
-        DEBUG ? System.getProperty("java.io.tmpdir") : System
-            .getProperty("user.dir");
+    public static final String WORKING_DIRECTORY = System.getProperty("user.dir");
     @Getter ModInfo info;
     @Getter Logger logger;
     @Getter Config config;
@@ -50,45 +48,25 @@ public abstract class Client implements IClient {
      * only be managed through singletons
      */
     protected Client(@NonNull final IEnvironment environment) {
-        UPTIME
-            .reset(); // intentional, as we want to only know exactly how long it has been since client started to load
+        UPTIME.reset();
 
         this.info = this.construct(environment);
         this.logger = new Logger(info.getName());
         val file = new File(WORKING_DIRECTORY, "${info.getName().toLowerCase()}.json");
-        if (DEBUG) {
-            file.deleteOnExit();
-        }
         this.config = new Config(file);
 
         logger.info("Running in environment: ${environment.getName()}");
-        if (DEBUG) {
-            logger.info("Starting %s in debug mode (no changes will be saved)",
-                this.getSimpleName());
-        }
+        logger.info("Starting %s in debug mode", this.getSimpleName());
     }
 
     /*
     This mainly exists due to the Fabric API loading classes but not resources until later, causing discrepancies with resources attempting to be loaded
      */
     private ModInfo construct(@NonNull final IEnvironment environment) {
-        if (DEBUG) {
-            val info = new JsonObject();
-            info.addProperty("name", "Developer");
-            info.addProperty("version", "0.0");
-            info.add(environment.getAuthorProperty(), new JsonArray());
+        val jar = new ZipFile(new File(StringUtils.substringBetween(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath(), "file:", "!").replace("%20", " ")));
+        val stream = jar.getInputStream(jar.getEntry(environment.getInfoFilename()));
+        val element = new JsonParser().parse(stream.bufferedReader());
 
-            return new ModInfo(info, environment);
-        }
-        else {
-            val jar = new ZipFile(new File(StringUtils
-                .substringBetween(
-                    this.getClass().getProtectionDomain().getCodeSource().getLocation()
-                        .getPath(), "file:", "!").replace("%20", " ")));
-            val stream = jar.getInputStream(jar.getEntry(environment.getInfoFilename()));
-            val element = new JsonParser().parse(stream.bufferedReader());
-
-            return new ModInfo(environment.getInfoJsonObject(element), environment);
-        }
+        return new ModInfo(environment.getInfoJsonObject(element), environment);
     }
 }
