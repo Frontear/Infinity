6bac635560b56b913978706eec15ad9ae0066a5d
Ali Rizvi <frontear4594@gmail.com>
2022-06-02 15:12:17 -0400

    bring back java executable WILL NOT BUILD
    currently efkolia is way out of date. i need to finalize maven publishing over there, and then this code will build correctly in dev. otherwise, the mod itself should work in normal environments.

--- src/main/java/com/github/frontear/InfinityLoader.java
+++ src/main/java/com/github/frontear/InfinityLoader.java
@@ -1,13 +1,17 @@
 package com.github.frontear;
 
 import com.github.frontear.efkolia.api.loader.ILoaderMod;
+import com.github.frontear.efkolia.utilities.file.JavaExecutable;
 import com.github.frontear.efkolia.utilities.timing.Timer;
 import com.github.frontear.infinity.InfinityMod;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.*;
 import java.util.Objects;
 import lombok.*;
+import net.fabricmc.loader.impl.launch.knot.KnotClient;
 
 public final class InfinityLoader implements ILoaderMod<InfinityMod> {
     public static final Timer UPTIME = new Timer();
@@ -22,12 +26,8 @@ public final class InfinityLoader implements ILoaderMod<InfinityMod> {
     public InfinityMod init(@NonNull final String... args) {
         UPTIME.reset();
 
-        //val executable = new JavaExecutable(InfinityLoader.class);
-        //mod = new InfinityMod(new JsonParser().parse(executable.getResource("fabric.mod.json")).getAsJsonObject());
-
-        val json = new JsonReader(new InputStreamReader(
-            Objects.requireNonNull(InfinityLoader.class.getResourceAsStream("/fabric.mod.json"))));
-        mod = new InfinityMod(JsonParser.parseReader(json).getAsJsonObject());
+        val executable = new JavaExecutable(InfinityLoader.class);
+        mod = new InfinityMod(JsonParser.parseReader(executable.getResource("fabric.mod.json")).getAsJsonObject());
 
         mod.getConfig().load();
 
