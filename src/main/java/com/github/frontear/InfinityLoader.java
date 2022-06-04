f72838901d0c0ef75a3a56ebaa97fb6f8952ca2c
Ali Rizvi <frontear4594@gmail.com>
2022-06-04 12:58:00 -0400

    update efkolia, fix java executable

--- src/main/java/com/github/frontear/InfinityLoader.java
+++ src/main/java/com/github/frontear/InfinityLoader.java
@@ -5,13 +5,8 @@ import com.github.frontear.efkolia.utilities.file.JavaExecutable;
 import com.github.frontear.efkolia.utilities.timing.Timer;
 import com.github.frontear.infinity.InfinityMod;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
-import java.io.*;
-import java.net.URISyntaxException;
-import java.nio.file.*;
-import java.util.Objects;
+import java.io.IOException;
 import lombok.*;
-import net.fabricmc.loader.impl.launch.knot.KnotClient;
 
 public final class InfinityLoader implements ILoaderMod<InfinityMod> {
     public static final Timer UPTIME = new Timer();
@@ -26,8 +21,13 @@ public final class InfinityLoader implements ILoaderMod<InfinityMod> {
     public InfinityMod init(@NonNull final String... args) {
         UPTIME.reset();
 
-        val executable = new JavaExecutable(InfinityLoader.class);
-        mod = new InfinityMod(JsonParser.parseReader(executable.getResource("fabric.mod.json")).getAsJsonObject());
+        try (val executable = new JavaExecutable(InfinityLoader.class)) {
+            mod = new InfinityMod(JsonParser.parseReader(executable.getResource("fabric.mod.json"))
+                .getAsJsonObject());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         mod.getConfig().load();
 
