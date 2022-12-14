48890e051e923b75cbad8f5c01c13dae0a1bc5ad
Ali Rizvi <frontear4594@gmail.com>
2022-12-14 12:30:32 -0500

    remove mixin, logger, modify jsons to explicitly state client mod

--- src/main/java/io/github/frontear/infinity/InfinityMod.java
+++ src/main/java/io/github/frontear/infinity/InfinityMod.java
@@ -1,14 +1,15 @@
 package io.github.frontear.infinity;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InfinityMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("Infinity");
-
+public class InfinityMod implements ClientModInitializer {
     @Override
-    public void onInitialize() {
-        LOGGER.info("Hello from Fabric!");
+    public void onInitializeClient() {} // exists to ensure fabric loads our mixins
+
+    public static Logger getLogger(String name) {
+        return LoggerFactory.getLogger(String.format("Infinity/%s", name));
     }
 }
