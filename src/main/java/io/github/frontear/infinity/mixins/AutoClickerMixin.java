c140d6e0ea9c96e0a2d4b68e9c51592a1beced13
Ali Rizvi <frontear4594@gmail.com>
2022-12-14 13:46:07 -0500

    remove the space between variables
    i will need to create a consistent file formatting guide

--- src/main/java/io/github/frontear/infinity/mixins/AutoClickerMixin.java
+++ src/main/java/io/github/frontear/infinity/mixins/AutoClickerMixin.java
@@ -15,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Minecraft.class)
 abstract class AutoClickerMixin {
     @Shadow @Nullable public HitResult hitResult;
-
     @Shadow @Nullable public LocalPlayer player;
 
     @Shadow protected abstract boolean startAttack();
