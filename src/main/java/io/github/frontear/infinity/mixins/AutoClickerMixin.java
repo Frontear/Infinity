93ab2301284531c22d66ab7ed30290a46b90a5d9
Ali Rizvi <frontear4594@gmail.com>
2022-12-14 13:45:32 -0500

    import parchment+mojang mappings, implement a AutoClicker
    I think, going forward, this is how I will implement things. Directly mixin them into the code, without the use of events and the like. This make's things less complicated to follow by having significantly less classes and files.

---
+++ src/main/java/io/github/frontear/infinity/mixins/AutoClickerMixin.java
@@ -0,0 +1,38 @@
+package io.github.frontear.infinity.mixins;
+
+import io.github.frontear.infinity.utils.AutoClicker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.phys.HitResult;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Minecraft.class)
+abstract class AutoClickerMixin {
+    @Shadow @Nullable public HitResult hitResult;
+
+    @Shadow @Nullable public LocalPlayer player;
+
+    @Shadow protected abstract boolean startAttack();
+
+    @Inject(at = @At("TAIL"), method = "startAttack", cancellable = true)
+    private void enableContinueAttackForEntityResult(CallbackInfoReturnable<Boolean> info) {
+        if (AutoClicker.isEnabled() && hitResult != null && hitResult.getType() == HitResult.Type.ENTITY) {
+            info.setReturnValue(true);
+        }
+    }
+
+    @Inject(at = @At("TAIL"), method = "continueAttack")
+    private void invokeStartAttackOnEntity(boolean leftClick, CallbackInfo info) {
+        if (AutoClicker.isEnabled() && leftClick && hitResult != null && hitResult.getType() == HitResult.Type.ENTITY) {
+            if (player != null && player.getAttackStrengthScale(0.0F) >= 1.0F) {
+                this.startAttack();
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/frontear/infinity/utils/AutoClicker.java
@@ -0,0 +1,7 @@
+package io.github.frontear.infinity.utils;
+
+public class AutoClicker {
+    public static boolean isEnabled() {
+        return true;
+    }
+}
