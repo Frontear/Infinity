ee1d4e3e4ec54160f5defd2c58d886addcf837a5
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-01-04 00:47:30 -0500

    dead change
    need to fix gpg keys

--- src/main/java/io/github/frontear/infinity/InfinityMod.java
+++ src/main/java/io/github/frontear/infinity/InfinityMod.java
@@ -6,7 +6,8 @@ import org.slf4j.LoggerFactory;
 
 public class InfinityMod implements ClientModInitializer {
     @Override
-    public void onInitializeClient() {} // exists to ensure fabric loads our mixins
+    public void onInitializeClient() {
+    } // exists to ensure fabric loads our mixins
 
     public static Logger getLogger(String name) {
         return LoggerFactory.getLogger(String.format("Infinity/%s", name));
