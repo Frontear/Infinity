8cc61bf5d83386f918fca40a182e870f4ceb5d8a
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-01-17 00:11:21 -0500

    implement a runtime tweak loader, and config for tweaks
    pretty big change, let's go into detail.
    
    First thing, for the dynamic loader, I opted to make use of Java.NIO FileSystems because (1) newer and modern API and (2) I've used it before in Efkolia and have a somewhat understanding. 
    
    Something that was very beneficial was the fact that we did NOT need to access the files, we just needed to find them and see their names (or paths technically, names were extracted from that). This allowed me to strip a huge chunk out of JavaExecutable from Efkolia and narrow down the specific methods that are necessary to traverse directories. It was important to ensure a jar check since (1) FileSystems fails when trying to pull a newFileSystem from the non-jar source, and (2) a non-jar should theoretically only be possible in a development environment, meaning the class data is in the gradle build folder, and can instead be treated just as a normal defaultFileSystem. I heavily encourage users to read what exactly FileSystem does and why it works the way it does for Zip/Jar files, and it would make more sense why we do what we do. From then on all we do is get the paths of all modules in the impl directory. Rather than hard-coding it I opted to use an already existing module to represent its package, which in this case was AutoClicker. After that its a matter of some string manipulation to create the name required by Class.forName, and we have successfully instantiated. Note that this approach only works in this very specific case. It's something that can only work for a jar that's inspecting itself, not for any other classes outside the jar. 
    
    The config is fairly trivial, by default I have set it so that instances are not created for the tweaks. Reason being Gson creates instances using its fromJson method, so I want to value those instances instead of my own. There is a safety catch within load that will populate null entries with freshly created instances. Afterwards, once loading is guaranteed finished, and the tweaks map is guaranteed populated, a shutdownHook is added to cause the tweak manager to save everything.

--- src/main/java/io/github/frontear/infinity/tweaks/TweakManager.java
+++ src/main/java/io/github/frontear/infinity/tweaks/TweakManager.java
@@ -1,24 +1,33 @@
 package io.github.frontear.infinity.tweaks;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import io.github.frontear.infinity.tweaks.impl.AutoClicker;
-import io.github.frontear.infinity.tweaks.impl.FullBright;
-import io.github.frontear.infinity.tweaks.impl.NoFOV;
-import io.github.frontear.infinity.tweaks.impl.SafeWalk;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class TweakManager {
-    private final static Map<Class<? extends AbstractTweak>, AbstractTweak> tweaks = new HashMap<>(); // TODO: memory implications with high object counts
+    private static final Map<Class<? extends AbstractTweak>, AbstractTweak> tweaks = new HashMap<>(); // TODO: memory implications with high object counts
+    private static final Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().serializeNulls().create();
+    private static final Path config = FabricLoader.getInstance().getConfigDir().resolve("infinity.json");
 
     static {
-        tweaks.put(AutoClicker.class, new AutoClicker());
-        tweaks.put(FullBright.class, new FullBright());
-        tweaks.put(NoFOV.class, new NoFOV());
-        tweaks.put(SafeWalk.class, new SafeWalk());
+        loadTweaks();
+        if (loadConfig()) {
+            Runtime.getRuntime().addShutdownHook(new Thread(TweakManager::saveConfig));
+        }
     }
 
     public static void handleKeyBinds(int key, int action) {
@@ -39,5 +48,81 @@ public class TweakManager {
         }
 
         throw new NoSuchElementException("Cannot find the specified tweak (" + type.getSimpleName() + "). You may have forgotten to initialize it in the static constructor.");
+    }
+
+    // pulling from what I did in Efkolia's JavaExecutable
+    private static void loadTweaks() {
+        try {
+            var source = Paths.get(TweakManager.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            var jar = source.getFileName().toString().endsWith(".jar");
+
+            var system = jar ? FileSystems.newFileSystem(source) : FileSystems.getDefault();
+            var pkg = AutoClicker.class.getPackageName();
+            var path = Paths.get("", pkg.split("\\."));
+
+            try (var walk = Files.walk(system.getPath((jar ? path : source.resolve(path)).toString())).skip(1)) { // skip directory itself
+                walk.forEach(x -> {
+                    var clazz = x.getFileName().toString();
+                    var name = pkg + "." + clazz.substring(0, clazz.length() - ".class".length());
+
+                    try {
+                        // set to null initially since gson populates them, and if not, it will populate later
+                        tweaks.put((Class<? extends AbstractTweak>) Class.forName(name), null);
+                    }
+                    catch (ClassNotFoundException e) {
+                        throw new Error(e); // should NOT happen
+                    }
+                });
+            }
+        }
+        catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean loadConfig() {
+        if (Files.exists(config)) {
+            try (var reader = Files.newBufferedReader(config)) {
+                var map = gson.fromJson(reader, JsonObject.class);
+
+                if (map != null) {
+                    for (var key : tweaks.keySet()) {
+                        var obj = map.get(key.getSimpleName());
+                        tweaks.putIfAbsent(key, gson.fromJson(obj, key));
+                    }
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for (var key : tweaks.keySet()) {
+            try {
+                if (tweaks.get(key) == null) { // different from putIfAbsent because constructor.newInstance doesn't need to be called _each_ time
+                    tweaks.put(key, key.getConstructor().newInstance());
+                }
+            }
+            catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return true;
+    }
+
+    private static void saveConfig() {
+        try (var writer = Files.newBufferedWriter(config)) {
+            var map = new JsonObject();
+
+            for (var key : tweaks.keySet()) {
+                map.add(key.getSimpleName(), gson.toJsonTree(tweaks.get(key)));
+            }
+
+            gson.toJson(map, writer);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
