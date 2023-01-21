b9cbb4f6af0a4cdcabe8d14baa2bffd076cc1aab
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-01-21 16:25:37 -0500

    add an autoclicker range cps delay
    this will provide some randomization to the attacks.

--- src/main/java/io/github/frontear/infinity/mixins/AutoClickerMixin.java
+++ src/main/java/io/github/frontear/infinity/mixins/AutoClickerMixin.java
@@ -29,14 +29,14 @@ abstract class AutoClickerMixin {
     @Inject(at = @At("TAIL"), method = "startAttack", cancellable = true)
     private void enableContinueAttackForEntityResult(CallbackInfoReturnable<Boolean> info) {
         if (autoClicker.isEnabled() && hitResult != null && hitResult.getType() == HitResult.Type.ENTITY) {
-            info.setReturnValue(true);
+            info.setReturnValue(true); // TODO: this doesn't seem to accomplish anything? strange considering the code bl3 |= startAttack should make continueAttack#leftClick false, and yet..?
         }
     }
 
     @Inject(at = @At("TAIL"), method = "continueAttack")
     private void invokeStartAttackOnEntity(boolean leftClick, CallbackInfo info) {
         if (autoClicker.isEnabled() && leftClick && hitResult != null && hitResult.getType() == HitResult.Type.ENTITY) {
-            if (player != null && player.getAttackStrengthScale(0.0F) >= 1.0F) {
+            if (player != null && player.getAttackStrengthScale(0.0F) >= 1.0F && autoClicker.canAttack()) {
                 this.startAttack();
             }
         }
--- src/main/java/io/github/frontear/infinity/tweaks/impl/AutoClicker.java
+++ src/main/java/io/github/frontear/infinity/tweaks/impl/AutoClicker.java
@@ -1,10 +1,47 @@
 package io.github.frontear.infinity.tweaks.impl;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import io.github.frontear.infinity.tweaks.AbstractTweak;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 public final class AutoClicker extends AbstractTweak {
+    @Expose
+    @SerializedName("minimumClicksPerSecond")
+    private int minCPS = 7;
+
+    @Expose
+    @SerializedName("maximumClicksPerSecond")
+    private int maxCPS = 11;
+
+    private long nanoTime = -1;
+    private long targetNanos = -1;
+
     public AutoClicker() {
         super(GLFW.GLFW_KEY_R);
+    }
+
+    public boolean canAttack() {
+        if (targetNanos == -1 || nanoTime == -1) {
+            var isFirstCall = targetNanos == -1;
+
+            // todo: assumption that max > min ALWAYS (orders are reversed because we reciprocate it)
+            targetNanos = (long) (ThreadLocalRandom.current().nextDouble(1.0 / maxCPS, 1.0 / minCPS) * TimeUnit.SECONDS.toNanos(1L));
+            nanoTime = System.nanoTime();
+
+            if (isFirstCall) {
+                return true; // otherwise run cps calculations
+            }
+        }
+
+        if (System.nanoTime() - nanoTime >= targetNanos) {
+            nanoTime = -1;
+            return true;
+        }
+
+        return false;
     }
 }
