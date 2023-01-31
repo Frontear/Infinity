57738ee8d555fbe7befe133991bc3f93dfbc9bce
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-01-31 16:46:04 -0500

    rewrite Xray for full compatibility with Sodium, and custom renderers
    These are very high-level changes, that could propagate into many unexpected places. As a result, these changes are very dangerous and could possibly cause some weird issues down the line. I will need to be vigilant when it comes to spotting bugs.

--- src/main/java/io/github/frontear/infinity/mixins/XrayMixin.java
+++ src/main/java/io/github/frontear/infinity/mixins/XrayMixin.java
@@ -1,79 +1,79 @@
 package io.github.frontear.infinity.mixins;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.serialization.MapCodec;
 import io.github.frontear.infinity.tweaks.TweakManager;
 import io.github.frontear.infinity.tweaks.impl.Xray;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.BlockRenderDispatcher;
-import net.minecraft.client.renderer.block.LiquidBlockRenderer;
-import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
-import net.minecraft.core.BlockPos;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.resources.model.MultiPartBakedModel;
+import net.minecraft.client.resources.model.SimpleBakedModel;
+import net.minecraft.client.resources.model.WeightedBakedModel;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.BlockGetter;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collections;
+import java.util.List;
+
+// TODO: the implications of such broad, high-level changes
 abstract class XrayMixin {
-    @Mixin(Minecraft.class)
-    static abstract class AOBypassMixin {
-        // TODO: implication of disabling at a global level
-        @Inject(method = "useAmbientOcclusion", at = @At("HEAD"), cancellable = true)
-        private static void disallowAmbientOcclusion(CallbackInfoReturnable<Boolean> info) {
+    @Mixin(BlockState.class)
+    static abstract class BlockMixin extends BlockBehaviour.BlockStateBase {
+        protected BlockMixin(Block block, ImmutableMap<Property<?>, Comparable<?>> immutableMap, MapCodec<BlockState> mapCodec) {
+            super(block, immutableMap, mapCodec);
+        }
+
+        @Override
+        public boolean canOcclude() {
             if (TweakManager.get(Xray.class).isEnabled()) {
-                info.setReturnValue(false);
+                return false;
             }
+
+            return super.canOcclude();
         }
     }
 
-    @Mixin(LiquidBlockRenderer.class)
-    static abstract class LiquidSideMixin {
-        @Inject(method = "isFaceOccludedByState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/shapes/Shapes;blockOccudes(Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/core/Direction;)Z"), cancellable = true)
-        private static void renderAllFaces(BlockGetter level, Direction face, float height, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> info) {
-            if (TweakManager.get(Xray.class).isEnabled()) {
-                info.setReturnValue(false);
-            }
-        }
-    }
+    @Mixin(FluidState.class)
+    static abstract class FluidMixin {
+        @Shadow
+        public abstract boolean is(Fluid fluid);
 
-    @Mixin(Block.class)
-    static abstract class BlockSideMixin {
-        @Inject(method = "shouldRenderFace", at = @At("HEAD"), cancellable = true)
-        private static void renderAllFaces(BlockState state, BlockGetter level, BlockPos offset, Direction face, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
-            if (TweakManager.get(Xray.class).isEnabled()) {
+        @Inject(method = "isEmpty", at = @At("HEAD"), cancellable = true)
+        private void skipRendering(CallbackInfoReturnable<Boolean> info) {
+            if (TweakManager.get(Xray.class).isEnabled() && (is(Fluids.WATER) || is(Fluids.FLOWING_WATER))) {
                 info.setReturnValue(true);
             }
         }
     }
 
-    @Mixin(ChunkRenderDispatcher.RenderChunk.RebuildTask.class)
-    static abstract class RenderDispatcherMixin {
-        private final Xray xray = TweakManager.get(Xray.class);
-
-        @Redirect(method = "compile", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getRenderShape()Lnet/minecraft/world/level/block/RenderShape;"))
-        private RenderShape skipBlockRendering(BlockState instance) {
-            if (xray.isEnabled() && !xray.isExcluded(instance.getBlock())) {
-                return RenderShape.INVISIBLE;
+    @Mixin({ MultiPartBakedModel.class, SimpleBakedModel.class, WeightedBakedModel.class })
+    static abstract class BakedModelMixin {
+        @Inject(method = "getQuads", at = @At("HEAD"), cancellable = true)
+        private void returnNoRenderingQuads(@Nullable BlockState state, @Nullable Direction direction, RandomSource random, CallbackInfoReturnable<List<BakedQuad>> info) {
+            if (state != null) {
+                if (TweakManager.get(Xray.class).isEnabled() && !TweakManager.get(Xray.class).isExcluded(state.getBlock())) {
+                    info.setReturnValue(Collections.emptyList());
+                }
             }
-
-            return instance.getRenderShape();
         }
 
-        @Redirect(method = "compile", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/BlockRenderDispatcher;renderLiquid(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/BlockAndTintGetter;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/material/FluidState;)V"))
-        private void ignoreLiquidRendering(BlockRenderDispatcher instance, BlockPos pos, BlockAndTintGetter level, VertexConsumer consumer, BlockState blockState, FluidState fluidState) {
-            if (xray.isEnabled() && (fluidState.is(Fluids.WATER) || fluidState.is(Fluids.FLOWING_WATER))) {
-                return;
+        @Inject(method = "useAmbientOcclusion", at = @At("HEAD"), cancellable = true)
+        private void disableAmbientOcclusion(CallbackInfoReturnable<Boolean> info) {
+            if (TweakManager.get(Xray.class).isEnabled()) {
+                info.setReturnValue(false);
             }
-
-            instance.renderLiquid(pos, level, consumer, blockState, fluidState);
         }
     }
 }
