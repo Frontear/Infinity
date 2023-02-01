66e35424c392edd2d090b89e83ae4da5d751c8e9
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-01-31 20:21:23 -0500

    transform tweak manager to use lazy loading
    This is a superior approach in all aspects. Honestly I wonder why I didn't do something like this before :p

--- src/main/java/io/github/frontear/infinity/tweaks/TweakManager.java
+++ src/main/java/io/github/frontear/infinity/tweaks/TweakManager.java
@@ -3,30 +3,52 @@ package io.github.frontear.infinity.tweaks;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import io.github.frontear.infinity.tweaks.impl.AutoClicker;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TweakManager {
-    private static final Map<Class<? extends AbstractTweak>, AbstractTweak> tweaks = new HashMap<>(); // TODO: memory implications with high object counts
+    private static final Map<String, JsonObject> config = new HashMap<>();
+    private static final Map<Class<? extends AbstractTweak>, AbstractTweak> tweaks = new HashMap<>();
     private static final Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().serializeNulls().create();
-    private static final Path config = FabricLoader.getInstance().getConfigDir().resolve("infinity.json");
 
     static {
-        loadTweaks();
+        var path = FabricLoader.getInstance().getConfigDir().resolve("infinity.json");
 
-        loadConfig();
-        Runtime.getRuntime().addShutdownHook(new Thread(TweakManager::saveConfig));
+        if (Files.exists(path)) {
+            try (var reader = Files.newBufferedReader(path)) {
+                var map = gson.fromJson(reader, JsonObject.class);
+
+                if (map != null) {
+                    for (var entry : map.entrySet()) {
+                        config.put(entry.getKey(), entry.getValue().getAsJsonObject());
+                    }
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (var writer = Files.newBufferedWriter(path)) {
+                var map = new JsonObject();
+
+                for (var tweak : tweaks.entrySet()) {
+                    map.add(tweak.getKey().getSimpleName(), gson.toJsonTree(tweak.getValue()));
+                }
+
+                gson.toJson(map, writer);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }));
     }
 
     public static void handleKeyBinds(int key, int action) {
@@ -43,80 +65,20 @@ public class TweakManager {
 
     // TODO: consider the problems with allowing access outside of the controlled environment
     public static <T extends AbstractTweak> T get(Class<T> key) {
+        if (!tweaks.containsKey(key)) {
+            var tweak = gson.fromJson(config.get(key.getSimpleName()), key);
+            if (tweak == null) {
+                try {
+                    tweak = key.getConstructor().newInstance();
+                }
+                catch (ReflectiveOperationException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            tweaks.put(key, tweak);
+        }
+
         return (T) tweaks.get(key);
-    }
-
-    // pulling from what I did in Efkolia's JavaExecutable
-    private static void loadTweaks() {
-        try {
-            var source = Paths.get(TweakManager.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-            var jar = source.getFileName().toString().endsWith(".jar");
-
-            var system = jar ? FileSystems.newFileSystem(source) : FileSystems.getDefault();
-            var pkg = AutoClicker.class.getPackageName();
-            var path = Paths.get("", pkg.split("\\."));
-
-            try (var walk = Files.walk(system.getPath((jar ? path : source.resolve(path)).toString())).skip(1)) { // skip directory itself
-                walk.forEach(x -> {
-                    var clazz = x.getFileName().toString();
-                    var name = pkg + "." + clazz.substring(0, clazz.length() - ".class".length());
-
-                    try {
-                        // set to null initially since gson populates them, and if not, it will populate later
-                        tweaks.put((Class<? extends AbstractTweak>) Class.forName(name), null);
-                    }
-                    catch (ClassNotFoundException e) {
-                        throw new Error(e); // should NOT happen
-                    }
-                });
-            }
-        }
-        catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void loadConfig() {
-        if (Files.exists(config)) {
-            try (var reader = Files.newBufferedReader(config)) {
-                var map = gson.fromJson(reader, JsonObject.class);
-
-                if (map != null) {
-                    for (var key : tweaks.keySet()) {
-                        var obj = map.get(key.getSimpleName());
-                        tweaks.putIfAbsent(key, gson.fromJson(obj, key));
-                    }
-                }
-            }
-            catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        for (var key : tweaks.keySet()) {
-            try {
-                if (tweaks.get(key) == null) { // different from putIfAbsent because constructor.newInstance doesn't need to be called _each_ time
-                    tweaks.put(key, key.getConstructor().newInstance());
-                }
-            }
-            catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static void saveConfig() {
-        try (var writer = Files.newBufferedWriter(config)) {
-            var map = new JsonObject();
-
-            for (var key : tweaks.keySet()) {
-                map.add(key.getSimpleName(), gson.toJsonTree(tweaks.get(key)));
-            }
-
-            gson.toJson(map, writer);
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
